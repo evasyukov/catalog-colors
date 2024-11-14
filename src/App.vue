0a02e0157a95ab96091711524022f0aaa9a02678
evasyukov <egor.vasyukov1@gmail.com>
2024-11-14 12:57:24 +0700

    Создание и настройка проекта

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <h1>Hello, Vue with Webpack!</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+}
+</script>
+
+<style scoped lang="scss">
+#app {
+  h1 {
+    color: #42b983;
+  }
+}
+</style>
---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+const path = require("path")
+const HtmlWebpackPlugin = require("html-webpack-plugin")
+const { VueLoaderPlugin } = require("vue-loader")
+const { CleanWebpackPlugin } = require("clean-webpack-plugin")
+
+module.exports = {
+  entry: "./src/main.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+  },
+  resolve: {
+    extensions: [".js", ".vue"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: "vue-loader",
+      },
+      {
+        test: /\.scss$/,
+        use: ["vue-style-loader", "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.css$/,
+        use: ["vue-style-loader", "css-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+    }),
+    new VueLoaderPlugin(),
+    new CleanWebpackPlugin(),
+  ],
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "public"),
+    },
+    compress: true,
+    port: 8080,
+    hot: true,
+  },
+}
