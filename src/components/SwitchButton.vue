901f50b3e7050c54e533c53fde3d82742d8e4083
evasyukov <egor.vasyukov1@gmail.com>
2024-11-18 19:36:25 +0700

    - доработан функционал переключателей - изменены стили переключателя

--- src/components/SwitchButton.vue
+++ src/components/SwitchButton.vue
@@ -1,58 +1,64 @@
 <template>
-    <div class="switch" @click="toggle">
-      <div class="circle" :class="{ active: isActive }"></div>
-    </div>
-  </template>
-  
-  <script setup>
-  import { ref, watch, defineProps, defineEmits } from 'vue';
-  
-  const props = defineProps({
-    modelValue: {
-      type: Boolean,
-      default: false,
-    },
-  });
-  
-  const emit = defineEmits(['update:modelValue']);
-  
-  const isActive = ref(props.modelValue);
-  
-  watch(
-    () => props.modelValue,
-    (newValue) => {
-      isActive.value = newValue;
-    }
-  );
-  
-  function toggle() {
-    isActive.value = !isActive.value;
-    emit('update:modelValue', isActive.value);
-  }
-  </script>
-  
-  <style scoped>
-  .switch {
-    width: 24px;
-    height: 24px;
-    border-radius: 50%;
-    background-color: #f0f0f0;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    cursor: pointer;
-  }
-  
+  <button class="switch" :class="{ active: modelValue }" @click="toggle">
+    <div class="circle"></div>
+  </button>
+</template>
+
+<script setup>
+import { defineProps, defineEmits } from "vue"
+
+const props = defineProps({
+  modelValue: {
+    type: Boolean,
+    default: false,
+  },
+})
+
+const emit = defineEmits(["update:modelValue"])
+
+function toggle() {
+  emit("update:modelValue", !props.modelValue)
+}
+</script>
+
+<style lang="scss" scoped>
+.switch {
+  width: 32px;
+  height: 22px;
+
+  border-radius: 40px;
+  border: none;
+
+  background-color: #f2f2f2;
+
+  display: flex;
+  align-items: center;
+
+  position: relative;
+
+  cursor: pointer;
+
+  transition: background-color 0.3s;
+
   .circle {
-    width: 12px;
-    height: 12px;
+    width: 8px;
+    height: 8px;
+
     border-radius: 50%;
-    background-color: #333;
-    transition: background-color 0.3s;
+    background-color: #1f2020;
+
+    position: absolute;
+    left: 7px;
+
+    transition: left 0.3s ease;
   }
-  
-  .circle.active {
-    background-color: #6AB89F; /* Цвет активного состояния */
+
+  &.active {
+    background-color: #7bb899;
+
+    .circle {
+      left: 17px;
+    }
   }
-  </style>
-  
+}
+</style>
